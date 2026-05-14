1c64f4e5b114b9398be1d7062e012fc2b3498356
EehMauro <maurojoaquinescudero@gmail.com>
2026-05-13 21:18:44 -0300

    feat: implement proof of concept for building the test tree using docker

---
+++ scripts/run-tests-json.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+VOLUME_NAME="pbt-extension-nix-store"
+
+docker volume create "$VOLUME_NAME" >/dev/null
+
+DOCKER_TTY_ARGS=("-i")
+if [ -t 0 ] && [ -t 1 ]; then
+  DOCKER_TTY_ARGS=("-it")
+fi
+
+docker run --rm "${DOCKER_TTY_ARGS[@]}" \
+  -v "$VOLUME_NAME:/nix" \
+  nixos/nix \
+  nix run \
+    --accept-flake-config \
+    --extra-experimental-features nix-command \
+    --extra-experimental-features flakes \
+    github:input-output-hk/sc-testing-tools#convex-testing-interface:test:convex-testing-interface-test \
+    -- --streaming-json
