f0d6d623620f9b9196da41e083cf069deabe1f12
EehMauro <maurojoaquinescudero@gmail.com>
2026-06-22 23:48:06 -0300

    fix server identations

--- server/index.ts
+++ server/index.ts
@@ -3,21 +3,21 @@ import * as rpc from 'vscode-jsonrpc/node';
 import { runScript } from './services/runScript';
 
 interface TestListResult {
-	event: 'suite_started';
-	tests: Array<TestListItem>;
+  event: 'suite_started';
+  tests: Array<TestListItem>;
 }
 
 interface TestListItem {
-	id: number;
-	name: string;
-	path: Array<string>;
-	srcLoc: {
-		file: string;
-		endCol: number;
-		endLine: number;
-		startCol: number;
-		startLine: number;
-	};
+  id: number;
+  name: string;
+  path: Array<string>;
+  srcLoc: {
+    file: string;
+    endCol: number;
+    endLine: number;
+    startCol: number;
+    startLine: number;
+  };
 };
 
 function isTestListResult(value: unknown): value is TestListResult {
@@ -25,35 +25,35 @@ function isTestListResult(value: unknown): value is TestListResult {
 }
 
 const buildTestList = async (): Promise<Array<Test>> => {
-	const testList: Array<Test> = [];
-	for await (const output of runScript('list-tests-json.sh')) {
-		if (isTestListResult(output.parsed)) {
-			const result = output.parsed as TestListResult;
-			for (const testItem of result.tests) {
-				testList.push({
-					id: testItem.id,
-					name: testItem.name,
-					group: testItem.path,
-					location: {
-						uri: testItem.srcLoc.file,
-						startLine: testItem.srcLoc.startLine,
-						startCharacter: testItem.srcLoc.startCol,
-						endLine: testItem.srcLoc.endLine,
-						endCharacter: testItem.srcLoc.endCol,
-					},
-					status: 'undetermined',
-				});
-			}
-		}
-	}
-	return testList;
+  const testList: Array<Test> = [];
+  for await (const output of runScript('list-tests-json.sh')) {
+    if (isTestListResult(output.parsed)) {
+      const result = output.parsed as TestListResult;
+      for (const testItem of result.tests) {
+        testList.push({
+          id: testItem.id,
+          name: testItem.name,
+          group: testItem.path,
+          location: {
+            uri: testItem.srcLoc.file,
+            startLine: testItem.srcLoc.startLine,
+            startCharacter: testItem.srcLoc.startCol,
+            endLine: testItem.srcLoc.endLine,
+            endCharacter: testItem.srcLoc.endCol,
+          },
+          status: 'undetermined',
+        });
+      }
+    }
+  }
+  return testList;
 };
 
 interface TestRun {
-	id: number;
-	event: 'test_done';
-	success: boolean;
-	duration: number;
+  id: number;
+  event: 'test_done';
+  success: boolean;
+  duration: number;
 }
 
 interface TestResult {
@@ -67,27 +67,27 @@ function isTestRun(value: unknown): value is TestRun {
 }
 
 const runTest = (testIds: number[]): void => {
-	(async () => {
-		for await (const output of runScript('run-tests-json.sh')) {
-			if (isTestRun(output.parsed)) {
-				const result = output.parsed as TestRun;
-				sendTestResult({
-					id: result.id,
-					status: result.success ? 'valid' : 'invalid',
-					time: result.duration * 1000,
-				});
-			}
-		}
-	})();
+  (async () => {
+    for await (const output of runScript('run-tests-json.sh')) {
+      if (isTestRun(output.parsed)) {
+        const result = output.parsed as TestRun;
+        sendTestResult({
+          id: result.id,
+          status: result.success ? 'valid' : 'invalid',
+          time: result.duration * 1000,
+        });
+      }
+    }
+  })();
 };
 
 const sendTestResult = (result: TestResult): void => {
-	connection.sendNotification('testResult', result);
+  connection.sendNotification('testResult', result);
 };
 
 const connection = rpc.createMessageConnection(
   new rpc.StreamMessageReader(process.stdin),
-	new rpc.StreamMessageWriter(process.stdout)
+  new rpc.StreamMessageWriter(process.stdout)
 );
 
 const buildTestListRequest = new rpc.RequestType<void, Array<Test>, void>('buildTestList');
