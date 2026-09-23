bc080ef95de5598fb7be5c6d110b099f3ee50dc9
KJES4 <kathrynjeanelizabethstacy@gmail.com>
2026-09-23 09:41:24 -0500

    fix for border issues in test fun config view

--- webview-ui/src/webview/testConfiguration/index.tsx
+++ webview-ui/src/webview/testConfiguration/index.tsx
@@ -82,7 +82,7 @@ const TestConfigurationView: React.FC<Props> = ({ vscode }) => {
             </VscodeRadioGroup>
             <VscodeTextfield
               id="rounds-per-test-textfield"
-              className="w-full"
+              className="w-full rounded-[var(--vscode-cornerRadius-small)] border-1 border-[var(--vscode-settings-textInputBorder)] focus:border-[var(--vscode-focusBorder)]"
               type="number"
               min={0}
               value={rounds}
